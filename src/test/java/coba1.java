5f751695ccc53eb5d195bd9421c5aa605cdbc680
Ridwan Setiawan <ridwan.tegar@next-ti.co.id>
2025-04-22 13:33:51 +0700

    Initial commit: Add AutomationAPI project files

---
+++ src/test/java/coba1.java
@@ -0,0 +1,15 @@
+//import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
+
+public class coba1 {
+
+    @Test
+    public void penjumlahan(){
+        System.out.println("TES");
+        assertEquals(2+2,4,"salah");
+    }
+
+}
